af815a472128b96729a8ab0b761553da4e5d2ade
hyunjinnam <jennifernam.dev@gmail.com>
2025-07-27 19:43:32 +1000

    fix backend oauth url

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-
+console.log('testing')
 
 const apiURL = process.env.API_URL;
 // Routes Handler
